b80952d2d61eb3846b3835dd551412f06441463c
LTBach <bach1346790852@gmail.com>
2026-01-28 12:06:02 +0000

    Add code

---
+++ code/model/__init__.py
@@ -0,0 +1,4 @@
+from .attention import BiAttention
+from .classifier import SimpleClassifier
+from .counting import Counter
+from .fc import FCNet, BCNet
---
+++ code/model/answer_net.py
@@ -0,0 +1,1 @@
+from answer_net.mlp import MLP
